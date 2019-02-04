e7c8129977f98ebde43405a70a01b397996e9b63
mbour <martin.bour@philips.com>
2019-02-04 17:12:01 -0500

    Working GET request returning XML response

--- src/eureka-api.js
+++ src/eureka-api.js
@@ -1,5 +1,23 @@
+const http = require('http');
+const log = require('./log');
+const fetch = require('node-fetch');
+const url = require('url');
+
+const conf = {
+    host: 'http://localhost:8765/eureka'
+}
+
 class EurekaAPI {
     apps () {
-        
+        log.info('EurekaAPI: ls');
+
+        const path = '/apps';
+        const apiUrl = new url.URL(conf.host + path);
+
+        return fetch(apiUrl, {
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
 }
+
+module.exports = EurekaAPI;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
+const EurekaAPI = require('./eureka-api');
 
 module.exports = {
     tunnel: require('./tunnel'),
-    log: require('./log')
+    log: require('./log'),
+    eureka: new EurekaAPI()
 }

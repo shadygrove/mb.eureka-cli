80ae36d829984dd3adb24b6ec12d1eb21e22d39b
mbour <martin.bour@philips.com>
2019-02-04 23:05:00 -0500

    Added GET commands for app-instance and instance

--- src/eureka-api.js
+++ src/eureka-api.js
@@ -8,10 +8,40 @@ const conf = {
 }
 
 class EurekaAPI {
-    apps (options) {
+    apps () {
         log.info('EurekaAPI: ls');
 
         const path = '/apps';
+        const apiUrl = new url.URL(conf.host + path);
+
+        return fetch(apiUrl, {
+                headers: { 'Accept': 'application/json' },
+            })
+            .then(res => res.json())
+            .then(body => (body));
+    }
+    app (appId) {
+        const path = `/apps/${appId}`;
+        const apiUrl = new url.URL(conf.host + path);
+
+        return fetch(apiUrl, {
+                headers: { 'Accept': 'application/json' },
+            })
+            .then(res => res.json())
+            .then(body => (body));
+    }
+    appInstance (appId, instanceId) {
+        const path = `/apps/${appId}/${instanceId}`;
+        const apiUrl = new url.URL(conf.host + path);
+
+        return fetch(apiUrl, {
+                headers: { 'Accept': 'application/json' },
+            })
+            .then(res => res.json())
+            .then(body => (body));
+    }
+    instance (instanceId) {
+        const path = `/instances/${instanceId}`;
         const apiUrl = new url.URL(conf.host + path);
 
         return fetch(apiUrl, {
